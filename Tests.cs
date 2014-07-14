29cbcd5b6b91c35faef8a560baa182fb1ca0c476
jesus- <chusma.ellagarto@gmail.com>
2014-07-14 12:22:57 +0800

    fix end lines

--- Tests.cs
+++ Tests.cs
@@ -9,8 +9,9 @@ namespace Aftership{
 
 		static void Main(string[] args)
 		{
-//			//Create an instace of ConnectionAPI using the token of the user
-			ConnectionAPI connection = new ConnectionAPI("60719d39-8de6-4092-9e2e-adfe19c7d2b5");
+
+			//Create an instance of ConnectionAPI using the token of the user
+			ConnectionAPI connection = new ConnectionAPI("a61d6204-6477-???-93ec-????????");
 
 			//create a new tracking to add to our account
 			Tracking newTracking = new Tracking ("7126900292");
@@ -27,7 +28,6 @@ namespace Aftership{
 			newTracking.trackingAccountNumber = "1234567";
 			newTracking.trackingPostalCode = "28046";
 			newTracking.trackingShipDate = "today";
-
 			newTracking.addCustomFields("price","1000");
 			newTracking.addCustomFields("product","iphone 5");
 
@@ -37,7 +37,6 @@ namespace Aftership{
 				//it will give you information why the transactions went wrong
 				if(connection.deleteTracking(newTracking))
 					Console.Write("Tracking deleted!!");
-
 			}catch(Exception e){
 				Console.Write (e.Message);
 			}
@@ -49,16 +48,9 @@ namespace Aftership{
 			//that the system have of the tracking (probably it won't have the
 			//Checkpoints, cause the system didn't have time to retrieve them from
 			//the couriers, so we can retrieve the Tracking
-
 			Tracking trackingGet = connection.getTrackingByNumber (trackingAdded);
-			Tracking trackingGet2 = new Tracking("7126900292");
-			trackingGet2.slug = "dhl";
-			Tracking trackingGet =  connection.getTrackingByNumber(trackingGet2);
-
-
 
 			//trackingGet will have all the information we want (including checkpoints)
-
 			// there is 2 ways of get the tracking:
 			//	* by id
 			//	* by slug, tracking number and optinally required params
@@ -67,7 +59,6 @@ namespace Aftership{
 			Tracking trackingGet1 = new Tracking("");//you dont care about tracking number
 			trackingGet1.id = "53be255bfdacaaae7b17834b";
 			Tracking tracking3 = connection.getTrackingByNumber(trackingGet1);
-
 
 			//example slug, tracking:
 			Tracking trackingGet2 = new Tracking("RC328021065CN");
@@ -79,7 +70,6 @@ namespace Aftership{
 			trackingGet3.slug = "deutsch-post";
 			trackingGet3.trackingShipDate = "20140627";
 			connection.getTrackingByNumber (trackingGet3);
-
 
 
 		}
